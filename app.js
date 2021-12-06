3125cefc754cf92e08d57da036fb91aed3828eb3
AgathePons <ponsagathe37@gmail.com>
2021-12-06 19:47:00 +0100

    add 404

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const server = express();
 const router = require('./app/router');
 server.use(router);
 
+server.use(function (req, res, next) {
+  res.status(404).send('pouet 404');
+});
+
 const PORT = 3000;
-server.listen(PORT, ()=>{
-    console.log(`Listening on ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
